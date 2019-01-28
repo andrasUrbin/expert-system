e2b28effc40a472057e48b2cb5ef0849ba6b9150
Berta Vincze <berta.vincze@gmail.com>
2019-01-28 11:05:58 +0100

    Added gitignore

--- src/main/java/FactIterator.java
+++ src/main/java/FactIterator.java
@@ -1,2 +1,24 @@
-public class FactIterator {
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class FactIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void forEachRemaining(Consumer action) {
+
+    }
 }
--- src/main/java/QuestionIterator.java
+++ src/main/java/QuestionIterator.java
@@ -1,2 +1,24 @@
-public class QuestionIterator {
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+public class QuestionIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void forEachRemaining(Consumer action) {
+
+    }
 }
